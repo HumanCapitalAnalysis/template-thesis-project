b30fee3023940986cebcce83f27728ce9ed7dbba
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2020-04-20 16:23:47 +0200

    fix

--- utils/travis_runner.py
+++ utils/travis_runner.py
@@ -1,12 +1,13 @@
-#!/bin/sh
-if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
-  wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
-else
-  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;\
-fi
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-hash -r
-conda config --set always_yes yes --set changeps1 no
-conda update -q conda
-conda info -a
+#!/usr/bin/env python
+"""This script manages all tasks for the TRAVIS build server."""
+import subprocess as sp
+import glob
+import os
+
+if __name__ == '__main__':
+
+    os.chdir('notebooks')
+
+    for notebook in glob.glob('*.ipynb'):
+        cmd = ' jupyter nbconvert --execute {}  --ExecutePreprocessor.timeout=-1'.format(notebook)
+        sp.check_call(cmd, shell=True)
